9d8d5b8dfc36164cf04a719606e57241ad319f46
timmySpark <timmyspark1@gmail.com>
2024-08-07 22:54:33 +0000

    [feat] :- importing from prev export functions and using them in a new function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,13 @@
-export default function uploadPhoto(filename) {
-  return Promise.reject(
-    new Error(`${filename} cannot be processed`),
-  );
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
