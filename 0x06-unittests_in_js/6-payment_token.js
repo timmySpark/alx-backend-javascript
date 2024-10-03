fea929f6601b8de8edd3636dddc03ac4547b513b
timmySpark <timmyspark1@gmail.com>
2024-10-03 18:45:05 +0000

    [feat] :- file fixes

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,12 +1,7 @@
-const { expect } = require('chai');
-const getPaymentTokenFromAPI = require('./6-payment_token');
-
-describe('getPaymentTokenFromAPI', () => {
-  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((res) => {
-        expect(res).to.deep.equal({ data: 'Successful response from the API' });
-        done();
-      });
-  });
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
 });
+
+module.exports = getPaymentTokenFromAPI;
